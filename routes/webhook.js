b4bb2ca1c06de89939edf7c36f2a5967df6bd744
Laxya Pahuja <laxya.pahuja8@gmail.com>
2021-02-22 23:51:01 +0530

    Delete user from unstars when the same user stars again

--- routes/webhook.js
+++ routes/webhook.js
@@ -4,82 +4,84 @@ const Repo = require('../models/Repo')
 
 const GITHUB_API_URL = 'https://api.github.com'
 
-router.get('/', async (req, res) => {
+router.get('/', async(req, res) => {
     const repo = req.query.repository
     const repoOwner = req.query.repoOwner
     const token = req.query.token
     const user = req.query.user
     Repo.findOne({ repoName: `${repoOwner}/${repo}` }).then((repository) => {
-      if(!repository) { 
-        const postData = { owner: `${repoOwner}`, repo: `${repo}`, events: ['star'], config: { url: `${process.env.BASE_URL}/webhook`, content_type: 'application/json; charset=utf-8' } }
-        const postConfig = {
-          method: 'post',
-          url: `${GITHUB_API_URL}/repos/${repoOwner}/${repo}/hooks`,
-          headers: { 
-            'Content-Type': 'application/json', 
-            'Authorization': `Bearer ${token}`
-          },
-          data : postData
-        };
-        axios(postConfig)
-        .then(response => {
-            console.log(response.data);
-            let newRepo = Repo({
-              webhookCreator: user,
-              repoName: `${repoOwner}/${repo}`,
-              webhookId: response.data.id
-            })
-            newRepo.save()
-            res.send({"message": "repo added"})
-            })
-        .catch(err => {
-            res.send({"message": "error"})
-            console.log(err);
-        })
-      } else {
-        res.send({"message": "repo already exists"})
-      }
+        if (!repository) {
+            const postData = { owner: `${repoOwner}`, repo: `${repo}`, events: ['star'], config: { url: `${process.env.BASE_URL}/webhook`, content_type: 'application/json; charset=utf-8' } }
+            const postConfig = {
+                method: 'post',
+                url: `${GITHUB_API_URL}/repos/${repoOwner}/${repo}/hooks`,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                data: postData
+            };
+            axios(postConfig)
+                .then(response => {
+                    console.log(response.data);
+                    let newRepo = Repo({
+                        webhookCreator: user,
+                        repoName: `${repoOwner}/${repo}`,
+                        webhookId: response.data.id
+                    })
+                    newRepo.save()
+                    res.send({ "message": "repo added" })
+                })
+                .catch(err => {
+                    res.send({ "message": "error" })
+                    console.log(err);
+                })
+        } else {
+            res.send({ "message": "repo already exists" })
+        }
     })
 });
 
 router.post('/', (req, res) => {
-  if(JSON.parse(req.body.payload) == undefined) {
-    res.send("hi")
-  } else {
-    const { sender, starred_at, action, repository } = JSON.parse(req.body.payload)
-    const repoName = repository.full_name
-    const userName = sender.login
-    const userId = sender.id
-    const deleted_at = new Date().toISOString()
-    let data = {}
-    Repo.findOne({ repoName: repoName }).then((repository) => {
-      if(!repository) {
-        res.send("can't find repo")
-      } else {
-        if(action === 'deleted') {
-          data = {
-            userName,
-            userId,
-            unstarredAt: deleted_at 
-          }
-          const initialStars = repository.stars
-          const updatedStars = initialStars.filter((user) => user.userId !== userId);
-          repository.stars = updatedStars
-          repository.unstars = [...repository.unstars, data]
-          repository.save()
-        } else {
-          data = {
-            userName,
-            userId,
-            starredAt: starred_at 
-          }
-          repository.stars = [...repository.stars, data]
-          repository.save()
-        }
-        res.send("updated")
-      }
-    })
-  }
+    if (JSON.parse(req.body.payload) == undefined) {
+        res.send("hi")
+    } else {
+        const { sender, starred_at, action, repository } = JSON.parse(req.body.payload)
+        const repoName = repository.full_name
+        const userName = sender.login
+        const userId = sender.id
+        const deleted_at = new Date().toISOString()
+        let data = {}
+        Repo.findOne({ repoName: repoName }).then((repository) => {
+            if (!repository) {
+                res.send("can't find repo")
+            } else {
+                if (action === 'deleted') {
+                    data = {
+                        userName,
+                        userId,
+                        unstarredAt: deleted_at
+                    }
+                    const initialStars = repository.stars
+                    const updatedStars = initialStars.filter((user) => user.userId !== userId);
+                    repository.stars = updatedStars
+                    repository.unstars = [...repository.unstars, data]
+                    repository.save()
+                } else {
+                    data = {
+                        userName,
+                        userId,
+                        starredAt: starred_at
+                    }
+                    const updatedUnstars = repository.unstars.filter((user) => user.userId !== userId);
+                    repository.unstars = updatedUnstars
+                    repository.stars = [...repository.stars, data]
+                    repository.save()
+                }
+                res.send("updated")
+            }
+        })
+    }
 })
 
 module.exports = router;
